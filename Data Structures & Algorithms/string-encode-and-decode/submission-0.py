5c73ce345f5aa7948f5a6fa6d1a197127daa4ca9
ibraheem shaikh <ibraheem.backup@gmail.com>
2026-06-10 15:17:44 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        output = ""
+        for i in range(len(strs) - 1):
+            output += strs[i]
+            output += '\n'
+        
+        # edgecase for last string
+        output += strs[len(strs) - 1]
+
+        return output 
+
+    def decode(self, s: str) -> List[str]:
+        word_num = 0
+        result = [""]
+        
+        for char in s:
+            if char == '\n':
+                word_num += 1
+                result.append("")
+            else:
+                result[word_num] += char
+
+        return result
