3a40908490297892cc67bdab57971c0cce7e1cd4
ibraheem shaikh <ibraheem.backup@gmail.com>
2026-06-11 16:08:28 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # concatinate all of these individual strings into a singular
+        # denote the length of each string 
+        output = ""
+
+        for s in strs:
+            output += str(len(s)) + "#" + s
+
+        return output
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            output.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+
+        return output
+
+
+
